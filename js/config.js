127d7196860e17c050c06444e300ae2b64ef1934
HD21Virtual <159862918+HD21Virtual@users.noreply.github.com>
2025-11-14 16:10:06 -0300

    Update and rename config.js.example to config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAbDQfS3VTVlXEBdHKKwx-ToTWTGFOcYAE",
+  authDomain: "vade-mecum-de-questoes.firebaseapp.com",
+  projectId: "vade-mecum-de-questoes",
+  storageBucket: "vade-mecum-de-questoes.firebasestorage.app",
+  messagingSenderId: "667396734608",
+  appId: "1:667396734608:web:96f67c131ccbd798792215"
+};
+
+export { firebaseConfig };
